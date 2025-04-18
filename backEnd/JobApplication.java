9aa611ae245215b09b0b13b41c0bb1746be62226
Jamie Ancog <jamieancog@gmail.com>
2025-04-18 14:27:42 -0700

    remove some print statements

--- backEnd/JobApplication.java
+++ backEnd/JobApplication.java
@@ -12,24 +12,19 @@ public class JobApplication {
     String requirements;
     Boolean isPrivate;
     String userComments;
-    Map<int, String> publicComments = new HashMap<>(); // (UserID to Comment)
+    Map<Integer, String> publicComments = new HashMap<>(); // (UserID to Comment)
     int authorID;
     String appStatus;
     int jobID;
 
     public JobApplication (
-        Company company, String jobPosition, String jobDescription, Long timeApplied, Long dueDate,
-        String requirements, Boolean isPrivate, String appStatus, int jobID
+        Company company, String jobPosition, int appId, Long timeApplied, String appStatus
     ) {
         this.company = company;
         this.jobPosition = jobPosition;
-        this.jobDescription = jobDescription;
         this.timeApplied = timeApplied;
-        this.dueDate = dueDate;
-        this.requirements = requirements;
-        this.isPrivate = isPrivate;
         this.appStatus = appStatus;
-        this.jobID = jobID;
+        this.appID = appID;
 
         setJobPosition(appId, jobPosition);
         setJobDescription(appId, jobDescription);
@@ -52,8 +47,8 @@ public class JobApplication {
     	setJobDescription(appId, newDescription);
     }
 
-    public void changePrivacy(boolean private) {
-    	isPrivate = private;
+    public void changePrivacy(boolean priv) {
+    	isPrivate = priv;
     }
 
     public void addPublicComment(int userID){
